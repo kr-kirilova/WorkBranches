3b1e3a48ca022effaccfd6f1219caa929195cd80
kr-kirilova <124675390+kr-kirilova@users.noreply.github.com>
2023-03-06 11:33:44 +0200

    Update Program.cs
    uuu

--- StatisticArray/StatisticArray/Program.cs
+++ StatisticArray/StatisticArray/Program.cs
@@ -6,8 +6,8 @@ namespace StatisticArray
     {
         static void Main(string[] args)
         {
-            //zadacha1
-        {cw
+           
+        {
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
