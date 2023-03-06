7b1ab81a68d97258e76f3858aacfc53d8c20f6c3
kr-kirilova <k.kirilova2019@pmg-pernik.com>
2023-03-06 11:26:56 +0200

    1

--- StatisticArray/StatisticArray/Program.cs
+++ StatisticArray/StatisticArray/Program.cs
@@ -5,7 +5,7 @@ namespace StatisticArray
     class Program
     {
         static void Main(string[] args)
-        {
+        {cw
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
